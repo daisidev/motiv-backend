21729b88cbe50ce05a4a5a264497143d01606bc2
daisidev <daisi@rootstudio.ng>
2025-08-23 21:28:51 +0100

    juwon deployed

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 
 	// Middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://localhost:3001, http://127.0.0.1:3000, https://motiv-azure.vercel.app, https://motiv-alpha-seven.vercel.app",
+		AllowOrigins:     "http://localhost:3000, http://localhost:3001, https://motiv-six.vercel.app, http://127.0.0.1:3000, https://motiv-azure.vercel.app, https://motiv-alpha-seven.vercel.app",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
